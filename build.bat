779f71552be303bbf4bdca6bceb03ef7a8ce13fc
Donald Hobern <dhobern@gmail.com>
2020-09-28 17:28:52 +1000

    Higher taxa

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
-java -jar ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar Alucitoidea coldp/data
+java -cp ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.CoLDPTool -i 4 -t Alucitoidea -T ../hobern.net/Alucitoidea/index.php -e Alucitoidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Alucitoidea-TEST.html
+
+java -cp ../CoLDPUtils/target/CoLDPUtils-1.0-SNAPSHOT-jar-with-dependencies.jar io.github.dhobern.coldp.CoLDPTool -i 4 -t Alucitoidea -xo ../hobern.net/Alucitoidea/Alucitoidea-catalogue.html coldp/data
+
 erase Alucitoidea.zip
 cd coldp
 7z a ../Alucitoidea.zip *
 cd ..
+
+copy Alucitoidea.zip ..\hobern.net\Alucitoidea
