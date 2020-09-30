78a50da2cd9c49987c28ca31b367f90bd20bcc48
Donald Hobern <dhobern@gmail.com>
2020-09-30 12:18:29 +1000

    New source for template file

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 erase Alucitoidea.zip
 cd coldp
 7z a ../Alucitoidea.zip *
@@ -6,6 +7,6 @@ cd ..
 copy Alucitoidea.zip ..\hobern.net\Alucitoidea
 
 
-.\coldp.bat TOHTML -i 4 -t Alucitoidea -T ../hobern.net/Alucitoidea/index.php -e Alucitoidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Alucitoidea-TEST.html
+.\coldp.bat TOHTML -i 4 -t Alucitoidea -T ../hobern.net/Alucitoidea/catalogue.php -e Alucitoidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Alucitoidea-TEST.html
 
 .\coldp.bat TOHTML -i 4 -t Alucitoidea -xo ../hobern.net/Alucitoidea/Alucitoidea-catalogue.html coldp/data
