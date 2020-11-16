1cf69b4c4da7bd5a1385ce92b3caaa29430cb25c
Donald Hobern <dhobern@gmail.com>
2020-11-16 17:29:43 +1100

    Recent updates from Bippus

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
-@ECHO OFF
+$versionid=get-date -UFormat "%y%j (%d %b %Y)"
+$releasedate=get-date -UFormat "%Y-%m-%d"
+sed -i -e "s/^version: .*$/version: 1.0.$versionid/" -e "s/^released: .*$/released: $releasedate/" coldp/metadata.yaml
+
 erase Alucitoidea.zip
 cd coldp
 7z a ../Alucitoidea.zip *
 cd ..
 
 copy Alucitoidea.zip ..\hobern.net\Alucitoidea
-
 
 .\coldp.bat TOHTML -w 4 -t Alucitoidea -T ../hobern.net/Alucitoidea/catalogue.php -e Alucitoidea-catalogue.html coldp/data | sed -e 's/\/img\//img\//' -e 's/\/css\//css\//' > ../hobern.net/Alucitoidea-TEST.html
 
